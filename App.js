7dd86a510ceb8b396db679498381c9a875b83c5b
David Zhang <davidzmh@gmail.com>
2023-07-16 23:17:30 -0700

    create basic app files

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable style={styles.button}>
+        <Text>Ask</Text>
+      </Pressable>
+      <Pressable style={styles.button}>
+        <Text>Answer</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +17,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'space-evenly'
   },
+
+  button: {
+    backgroundColor: 'aliceBlue',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+  }
 });
